637b0efa7922e877856ad6dd1221d4dbbcecfe6b
Cecilia <cecilia92fernandez@gmail.com>
2025-11-09 15:15:00 -0300

    se creo archivo js de recupero de contraseña

---
+++ js/recperar-password.js
@@ -0,0 +1,9 @@
+/* FORMULARIO 3: recuperar-password.html 
+o Para los campos recuadrados en rojo se pide validar que no estén vacíos (carga obligatoria) 
+o El botón CONTINUAR debe encontrarse deshabilitado (no responder al clic) mientras no se hayan 
+completados TODOS los campos de carga obligatoria, y habilitarse una vez completados TODOS esos 
+campos. En caso de que se borre el contenido en estos campos (nuevamente vacío), volver a 
+deshabilitar el botón. 
+o VALIDAR el contenido de los campos que se indican debajo: 
+ EMAIL: formato válido de mail (un @ en el medio) y que termine con .com, .org o .net */
+
